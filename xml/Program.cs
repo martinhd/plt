3319f97bb35d0d69280641ffaf505e0ad4e9af99
Ubuntu <ubuntu@ip-172-31-53-57.ec2.internal>
2018-05-05 13:47:01 +0000

    small additions

--- xml/Program.cs
+++ xml/Program.cs
@@ -2,15 +2,21 @@
 using System.Xml;
 using System.Xml.XPath;
 
+//see https://msdn.microsoft.com/en-us/library/ms256086(v=vs.110).aspx
+
 namespace xml
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            XPathDocument doc = new XPathDocument("books.xml");
+            XPathDocument doc = new XPathDocument("https://www.w3schools.com/xml/books.xml");
             var nav = doc.CreateNavigator();
+            
+            var authors = nav.Select("/bookstore/book/author[1]");
+
+            foreach(var author in authors)
+               Console.WriteLine(author.ToString());
         }
     }
 }
