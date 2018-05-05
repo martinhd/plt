af2c0bc60596d482d4d09c98f68dd4a821f2d2e4
Ubuntu <ubuntu@ip-172-31-53-57.ec2.internal>
2018-05-05 13:28:40 +0000

    initial

---
+++ xml/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            XPathDocument doc = new XPathDocument("books.xml");
+            var nav = doc.CreateNavigator();
+        }
+    }
+}
